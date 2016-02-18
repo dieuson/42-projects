2c60ba8ee83b8904b4e931d76799e5fc1ed43ff6
Dieuson VIRGILE <dvirgile@e3r7p11.42.fr>
2016-02-18 07:48:58 +0100

    get next line

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: dvirgile <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/30 13:38:27 by dvirgile          #+#    #+#             */
-/*   Updated: 2016/02/02 12:42:34 by dvirgile         ###   ########.fr       */
+/*   Updated: 2016/02/09 09:48:00 by dvirgile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include "./libft/includes/libft.h"
-# define BUFF_SIZE 2
+# define BUFF_SIZE 16
 
 typedef struct		s_docker
 {
